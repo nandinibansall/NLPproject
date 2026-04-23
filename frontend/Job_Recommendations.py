4840677abd4fceec09dcc0b9e3c019d1b77d9a20
kopalg20 <kopalgarg2005@gmail.com>
2026-04-23 20:57:55 +0530

    fix:add deployed version

--- frontend/Job_Recommendations.py
+++ frontend/Job_Recommendations.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BACKEND_URL = "http://127.0.0.1:5000/api"
+BACKEND_URL = "https://nlpprojectbackend.onrender.com/api"
 
 st.set_page_config(page_title="Career Assistant", layout="wide")
 
